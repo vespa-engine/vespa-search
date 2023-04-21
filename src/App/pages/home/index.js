92e9fc7fb0209a1b6ffbb822651514cf30c38afd
Valerij Fredriksen <valerijf@yahooinc.com>
2023-04-21 14:28:03 +0200

    Fix autocomplete

--- src/App/pages/home/index.js
+++ src/App/pages/home/index.js
@@ -3,7 +3,6 @@ import { Container, Space, Stack } from '@mantine/core';
 import { SearchInput } from 'App/pages/search/search-input.js';
 import { ENDPOINT } from 'App/pages/search/index.js';
 import { SearchSources } from 'App/pages/home/search-sources.js';
-import { SearchAutocomplete } from 'App/pages/search/search-autocomplete';
 
 export function Home() {
   return (
@@ -11,7 +10,6 @@ export function Home() {
       <Space h={55} />
       <Stack>
         <SearchInput endpoint={ENDPOINT} size="lg" />
-        <SearchAutocomplete endpoint={ENDPOINT} size="lg" />
         <SearchSources />
       </Stack>
     </Container>
--- src/App/pages/search/search-input.js
+++ src/App/pages/search/search-input.js
@@ -1,34 +1,100 @@
-import React, { useState } from 'react';
-import { TextInput, ActionIcon } from '@mantine/core';
+import React, { forwardRef, useEffect, useState } from 'react';
+import { ActionIcon, Autocomplete, Group, Text } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
-import { Icon, Keyboard } from 'App/components/index.js';
+import { UrlBuilder } from 'App/utils';
+import { Get } from 'App/libs/fetcher';
+import { Icon } from 'App/components/index.js';
+import { Link } from 'App/libs/router';
 
-export function SearchInput({ query, size = 'md' }) {
+const AutoCompleteItem = forwardRef(({ value, type, url, ...props }, ref) => {
+  return (
+    <div ref={ref} {...props}>
+      <Group position="apart" noWrap>
+        <Text>{value}</Text>
+        {type && <Link to={url}>{type}</Link>}
+      </Group>
+    </div>
+  );
+});
+
+export function SearchInput({ endpoint, query, size = 'md' }) {
   const navigate = useNavigate();
+  const [dropdownOpened, setDropdownOpened] = useState(false);
   const [value, setValue] = useState(query ?? '');
-  const doSearch = () => navigate(`/search?q=${value}`);
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const alive = { current: true };
+    if (value.length === 0) {
+      setSuggestions([]);
+      return;
+    }
+
+    Get(new UrlBuilder(endpoint).add('suggest').queryParam('query', value))
+      .then((response) =>
+        setSuggestions(
+          (response?.root?.children ?? []).map((item) => ({
+            value: item.fields.term,
+            type: item.fields.type,
+            url: item.fields.url,
+          }))
+        )
+      )
+      .catch(() => setSuggestions([]));
+
+    return () => {
+      alive.current = false;
+    };
+  }, [endpoint, value]);
+
+  const onSubmit = ({ value, url }) => {
+    url ? (location.href = url) : navigate(url ?? `/search?q=${value}`);
+  };
 
   return (
-    <Keyboard onEnter={doSearch}>
-      <TextInput
-        icon={<Icon name="magnifying-glass" />}
-        rightSection={
-          <ActionIcon
-            onClick={doSearch}
-            variant="filled"
-            color="blue"
-            radius="xl"
-            size="lg"
-          >
-            <Icon name="arrow-right" />
-          </ActionIcon>
-        }
-        placeholder="Ask a question about Vespa"
-        onChange={(event) => setValue(event.currentTarget.value)}
-        value={value}
-        size={size}
-        radius="xl"
-      />
-    </Keyboard>
+    <Autocomplete
+      styles={(theme) => ({
+        input: {
+          ...(dropdownOpened &&
+            suggestions.length > 0 && {
+              borderBottomLeftRadius: 0,
+              borderBottomRightRadius: 0,
+              '&:focus, &:focus-within': {
+                borderBottomColor: 'transparent',
+              },
+            }),
+        },
+        dropdown: {
+          ...theme.focusRingStyles.inputStyles(theme),
+          borderBottomLeftRadius: theme.radius.xl,
+          borderBottomRightRadius: theme.radius.xl,
+          borderTop: 'none',
+          marginTop: -10,
+        },
+      })}
+      onDropdownOpen={() => setDropdownOpened(true)}
+      onDropdownClose={() => setDropdownOpened(false)}
+      icon={<Icon name="magnifying-glass" />}
+      itemComponent={AutoCompleteItem}
+      rightSection={
+        <ActionIcon
+          onClick={() => onSubmit({ value })}
+          variant="filled"
+          color="blue"
+          radius="xl"
+          size="lg"
+        >
+          <Icon name="arrow-right" />
+        </ActionIcon>
+      }
+      placeholder="Ask a question about Vespa"
+      data={suggestions}
+      onChange={setValue}
+      onItemSubmit={onSubmit}
+      value={value}
+      size={size}
+      filter={() => true}
+      radius="xl"
+    />
   );
 }
