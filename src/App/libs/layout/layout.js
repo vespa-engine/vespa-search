bab44d27cab4af89d696dc6b13306ed7d1e5b3ff
leandroalves <leandroalves@yahooinc.com>
2023-04-25 13:51:00 +0200

    Fix main app max width

--- src/App/libs/layout/layout.js
+++ src/App/libs/layout/layout.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { AppShell } from '@mantine/core';
 import { Header } from 'App/libs/layout/header';
+import { Container } from 'App/components';
 
 export function Layout({ children }) {
-  return <AppShell header={<Header />}>{children}</AppShell>;
+  return (
+    <AppShell header={<Header />}>
+      <Container withMaxWidth>{children}</Container>
+    </AppShell>
+  );
 }
