03d7678dd531b43b7a59547f8da43ea456624a8f
Valerij Fredriksen <valerijf@yahooinc.com>
2023-10-13 10:48:13 +0200

    Parse md in UI

--- src/App/pages/search/abstract-container/abstract/abstract-content.js
+++ src/App/pages/search/abstract-container/abstract/abstract-content.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Stack } from '@mantine/core';
 import { useSearchContext } from 'App/libs/provider/index.js';
+import { parseMarkdown } from 'App/pages/search/md-parser.js';
 
 export function AbstractContent() {
-  const summary = useSearchContext((ctx) => ctx.summary.element);
-  return <Stack>{summary}</Stack>;
+  const summary = useSearchContext((ctx) => ctx.summary.raw);
+  return <Stack>{parseMarkdown(summary)}</Stack>;
 }
