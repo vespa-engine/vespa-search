ab33a3b1523fb6709e3e4970a92a7d8b17af2018
leandroalves <leandroalves@yahooinc.com>
2024-08-26 11:57:14 +0200

    Link to documentation

--- src/App/pages/home/index.js
+++ src/App/pages/home/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Anchor, Container, Group, Space } from '@mantine/core';
+import { Container, Group, Space } from '@mantine/core';
 import { SearchInput } from 'App/pages/search/search-input/index.js';
 import { Icon } from 'App/components/index.js';
+import { Link } from 'App/libs/router/index.js';
 
 export function Home() {
   return (
@@ -13,19 +14,12 @@ export function Home() {
       <Container>
         <Space h={50} />
       </Container>
-      {/* eslint-disable-next-line no-undef */}
-      <div style={{ margin: 'auto', width: '50%' }}>
-        <Anchor
-          href={'https://docs.vespa.ai/'}
-          //size={isMobile ? 'xs' : 'sm'} we should fix this, too
-          target="_blank"
-        >
-          <Group gap="xs">
-            <Icon name="external-link" />
-            Browse documentation
-          </Group>
-        </Anchor>
-      </div>
+      <Link to="https://docs.vespa.ai">
+        <Group gap="xs" justify="center">
+          <Icon name="external-link" />
+          Browse documentation
+        </Group>
+      </Link>
     </div>
   );
 }
